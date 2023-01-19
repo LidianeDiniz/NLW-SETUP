102ecc8a7a0cf3d1ea90524745207ac2751ec1b3
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2023-01-19 20:35:31 -0300

    Inicializando o projeto

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import './styles/global.css';
+import { Habit } from "./components/Habit"
+
+
+ function App() {
+
+  return (
+    <div>
+      <Habit/>
+      <Habit/>
+      <Habit/>
+      <Habit/>
+    </div>
+  )
+  }
+    
+
+export default App
---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,5 @@
+export function Habit(){
+    return (
+        <p>Habit</p>
+    )
+}
